543bd6f2db4c73d9473dc3b016c0646ca97c791f
Eugene <korzh367@gmail.com>
2025-11-01 19:20:00 +0100

    done

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,14 +1,14 @@
 import { Metadata } from "next";
 export const metadata: Metadata = {
-  title: "Page Not Found | NoteHub",
+  title: "Page Not Found",
   description: "The page you are looking for does not exist on NoteHub.",
   openGraph: {
-    title: "Page Not Found | NoteHub",
+    title: "Page Not Found",
     description: "The page you are looking for does not exist on NoteHub.",
     url: "https://08-zustand-gamma-henna.vercel.app/not-found",
     images: [
       {
-        url: "https://stories.freepiklabs.com/storage/13391/355-404-Error_Artboard-1.svg",
+        url: "https://08-zustand-gamma-henna.vercel.app/images/404.png",
         width: 1200,
         height: 630,
         alt: "404 Page Not Found",
